219e46606cc6bce0e677625f4c58afa5c487a8ee
LukasVN <101676436+LukasVN@users.noreply.github.com>
2024-04-03 21:20:59 +0200

    Started to work in v3.0 version (Trying to not repeat colors)

--- Assets/ColorSelectionNetwork/Scripts/NetworkPlayer.cs
+++ Assets/ColorSelectionNetwork/Scripts/NetworkPlayer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -5,6 +6,7 @@ public class NetworkPlayer : NetworkBehaviour
 {
     public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>(); //Network position for each NetworkPlayer
     private NetworkVariable<int> MaterialIndex = new NetworkVariable<int>(); //Network Material Index for each NetworkPlayer
+    private static List<int> MaterialIndexList = new List<int>(); //Material Index List for each NetworkPlayer
     public Material[] colorMaterials;
     private MeshRenderer meshRenderer;
 
@@ -40,11 +42,19 @@ public class NetworkPlayer : NetworkBehaviour
         [Rpc(SendTo.Server)]
         void SubmitColorRequestServerRpc(RpcParams rpcParams = default)
         {
+            if(MaterialIndexList.Count == 6){
+                return;
+            }
+            
             int randomIndex = Random.Range(0,colorMaterials.Length);
             while(randomIndex == MaterialIndex.Value){
+                if(MaterialIndexList.Contains(randomIndex)){
+                    continue;
+                }
                 randomIndex = Random.Range(0,colorMaterials.Length);
             }
             MaterialIndex.Value = randomIndex; //Asigns network color index for client's player meshRenderer
+            MaterialIndexList.Add(randomIndex);
             meshRenderer.material = colorMaterials[MaterialIndex.Value];
         }
 
@@ -57,6 +67,7 @@ public class NetworkPlayer : NetworkBehaviour
         {
             transform.position = Position.Value;
             GetComponent<MeshRenderer>().material =  colorMaterials[MaterialIndex.Value];
+            
         }
-    }
+}
 
